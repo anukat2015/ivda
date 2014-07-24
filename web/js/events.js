b1a1cac3e9f867cd27e8be568dd84220c7a782b0
Sekerak <sekys.sekys@gmail.com>
2014-07-24 17:44:57 +0200

    Upraveny time zone pre odosielany cas. Pridana datatable pre debugovanie.

--- web/js/events.js
+++ web/js/events.js
@@ -1,32 +1,61 @@
+function getSelectedRow() {
+    var row = undefined;
+    var sel = gTimeline.getSelection();
+    if (sel.length) {
+        if (sel[0].row != undefined) {
+            row = sel[0].row;
+        }
+    }
+    return row;
+}
 
 function onSelect() {
-	var row = getSelectedRow();
-	if (row == undefined) return;
-	var item = gData.getValue(row, 2);
-	console.log("item " + item + " selected");
+    var row = getSelectedRow();
+    if (row == undefined) return;
+    var item = gData.getValue(row, 2);
+    console.log("item " + item + " selected");
+}
+
+// Format given date as "yyyy-mm-dd hh:ii:ss"
+// @param datetime   A Date object.
+function dateFormat(date) {
+    var datetime = date.getFullYear() + "-" +
+        ((date.getMonth() < 9) ? "0" : "") + (date.getMonth() + 1) + "-" +
+        ((date.getDate() < 10) ? "0" : "") + date.getDate() + " " +
+        ((date.getHours() < 10) ? "0" : "") + date.getHours() + ":" +
+        ((date.getMinutes() < 10) ? "0" : "") + date.getMinutes() + ":" +
+        ((date.getSeconds() < 10) ? "0" : "") + date.getSeconds();
+    return datetime;
 }
 
 function onRangeChange() {
-	var range = gTimeline.getVisibleChartRange();
-	document.getElementById('startDate').value = dateFormat(range.start);
-	document.getElementById('endDate').value   = dateFormat(range.end);
+    var range = gTimeline.getVisibleChartRange();
+    document.getElementById('startDate').value = dateFormat(range.start);
+    document.getElementById('endDate').value = dateFormat(range.end);
 }
-
 function onRangeChanged() {
-	var range = gTimeline.getVisibleChartRange();
-	console.log("range changed" + dateFormat(range.start) + " " + dateFormat(range.end));
-	loadRange(range.start, range.end, gTimeline.size.contentWidth);
+    var range = gTimeline.getVisibleChartRange();
+    console.log("onRangeChanged     LOCAL FORMAT " + gDateFormater.formatValue(range.start) + " " + gDateFormater.formatValue(range.end));
+    console.log("                   ISO FORMAT " + range.start.toISOString() + " " + range.end.toISOString());
+    loadRange(range.start, range.end, gTimeline.size.contentWidth);
 }
 
 function setTime() {
-	if (!gTimeline) return;
-	var newStartDate = new Date(document.getElementById('startDate').value);
-	var newEndDate   = new Date(document.getElementById('endDate').value);
-	gTimeline.setVisibleChartRange(newStartDate, newEndDate);
+    if (!gTimeline) return;
+    var newStartDate = new Date(document.getElementById('startDate').value);
+    var newEndDate = new Date(document.getElementById('endDate').value);
+    gTimeline.setVisibleChartRange(newStartDate, newEndDate);
+    onRangeChange();
+    onRangeChanged();
 }
 
 function setCurrentTime() {
-	if (!gTimeline) return;
-	gTimeline.setVisibleChartRangeNow();
-	onRangeChange();
+    if (!gTimeline) return;
+    gTimeline.setVisibleChartRangeNow();
+    onRangeChange();
+    onRangeChanged();
+}
+
+function onReSize() {
+    gTimeline.redraw();
 }
--- web/js/timeline.js
+++ web/js/timeline.js
@@ -1,62 +1,75 @@
 // Called when the Visualization API is loaded.
 
 function drawChart() {
-	var data = google.visualization.arrayToDataTable([
-	  ['Task', 'Hours per Day'],
-	  ['Work',     11],
-	  ['Eat',      2],
-	  ['Commute',  2],
-	  ['Watch TV', 2],
-	  ['Sleep',    7]
-	]);
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+    ]);
 
-	var options = {
-	  title: 'My Daily Activities'
-	};
+    var options = {
+        title: 'My Daily Activities'
+    };
 
-	var gChart = new google.visualization.PieChart(document.getElementById('piechart'));
-	gChart.draw(data, options);
+    var gChart = new google.visualization.PieChart(document.getElementById('piechart'));
+    gChart.draw(data, options);
 }
-	  
-function drawVisualization() {
-	// Create and populate a data table.
-    gData = new google.visualization.DataTable();
-	var now = new Date();
-	var start = new Date(now.getTime() - 4 * 60 * 60 * 1000);
-	var end = new Date(now.getTime() + 8 * 60 * 60 * 1000);
-    //loadRange(start, end);
 
-		
-	// specify options
-	var options = {
-		width:  "100%",
-		height: "50%",
-		layout: "box",
-		axisOnTop: true,
-		eventMargin: 1,  // minimal margin between events
-		eventMarginAxis: 0, // minimal margin beteen events and the axis
-		editable: false,
-		showNavigation: true,
-		zoomMin: 1000 * 60 * 5,             // 15sec
-		zoomMax: 1000 * 60 * 60 * 24 * 7,     // tyzden
-		animate: true,
-		animateZoom: true,
-		//cluster: true,
-		groupMinHeight: 100	
-	};
+function convertDateToUTC(date) {
+    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+}
 
-	// Instantiate our timeline object.
-	gTimeline = new links.Timeline(document.getElementById('mytimeline'), options);
+function createDateAsUTC(date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+}
+
+function inicializeTimeline(nowDate) {
+    // Nastavenia timeline
+    var options = {
+        width: "100%",
+        height: "50%",
+        layout: "box",
+        axisOnTop: true,
+        eventMargin: 1,  // minimal margin between events
+        eventMarginAxis: 0, // minimal margin beteen events and the axis
+        editable: false,
+        showNavigation: true,
+        zoomMin: 1000 * 60 * 1,             // 5sec
+        zoomMax: 1000 * 60 * 60 * 24 * 7,     // tyzden
+        animate: true,
+        animateZoom: true,
+        //cluster: true,
+        groupMinHeight: 100
+    };
+
+    // Instantiate our timeline object.
+    gTimeline = new links.Timeline(document.getElementById('mytimeline'), options);
     google.visualization.events.addListener(gTimeline, 'rangechange', onRangeChange);
     google.visualization.events.addListener(gTimeline, 'rangechanged', onRangeChanged);
-	google.visualization.events.addListener(gTimeline, 'select', onSelect);
-	
-	gTimeline.draw(gData);
-	gTimeline.setVisibleChartRange(start, end);
-	onRangeChange();
+    google.visualization.events.addListener(gTimeline, 'select', onSelect);
+
+    // Nastav hlavny cas odkedy budeme hladat eventy
+    var start = new Date(nowDate.getTime() - 5 * 60 * 60 * 1000); // poslednych 5 min
+    var end = nowDate;
+    gTimeline.draw();
+    gTimeline.setVisibleChartRange(start, end);
+    onRangeChange();
     onRangeChanged();
-	
-	drawChart();
+}
+
+function drawVisualization() {
+    // Inicializuj data
+    gDateFormater = new google.visualization.DateFormat({formatType: 'medium'});
+    gDataTable = new google.visualization.Table(document.getElementById('datatable'));
+    gData = new google.visualization.DataTable();
+
+    var start = new Date("2014-07-23T14:47:12.000");    // debug
+    //var start = new Date();
+    inicializeTimeline(start);
+    drawChart();
 }
 
 function handleServiceResponse(response) {
@@ -65,13 +78,24 @@ function handleServiceResponse(response) {
         return;
     }
     gData = response.getDataTable();
-    console.log(gData);
+    //console.log(gData);
     gTimeline.draw(gData);
+    gDataTable.draw(gData, {
+        allowHtml: true,
+        showRowNumber: true,
+        page: "enable",
+        pageSize: 20
+    });
 }
 
 
 function loadRange(start, end, width) {
-    var query = new google.visualization.Query('datatable?start=' + start.getTime() + '&end=' + end.getTime() + '&width=' + width);
+    var parameters = $.param({
+        start: createDateAsUTC(start).toISOString(),
+        end: createDateAsUTC(end).toISOString(),
+        width: width
+    });
+    var query = new google.visualization.Query("datatable?" + parameters);
     query.send(handleServiceResponse);
 }
 /*
