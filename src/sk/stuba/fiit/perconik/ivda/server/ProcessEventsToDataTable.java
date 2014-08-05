6a0203f52cc3c3958f6cd9df4d45a7f5a867dcc3
Sekerak <sekys.sekys@gmail.com>
2014-08-05 15:39:57 +0200

    Vymazane nepotrebne poznamky ku kodu. Vytvoreny kod na analyzu ASTRCS sluzby.

--- src/sk/stuba/fiit/perconik/ivda/server/ProcessEventsToDataTable.java
+++ src/sk/stuba/fiit/perconik/ivda/server/ProcessEventsToDataTable.java
@@ -3,8 +3,7 @@ package sk.stuba.fiit.perconik.ivda.server;
 import com.google.visualization.datasource.base.TypeMismatchException;
 import com.gratex.perconik.services.AstRcsWcfSvc;
 import com.gratex.perconik.services.IAstRcsWcfSvc;
-import com.gratex.perconik.services.ast.rcs.GetUserRequest;
-import com.gratex.perconik.services.ast.rcs.GetUserResponse;
+import com.gratex.perconik.services.ast.rcs.*;
 import com.ibm.icu.util.GregorianCalendar;
 import com.ibm.icu.util.TimeZone;
 import org.apache.log4j.Logger;
@@ -26,20 +25,20 @@ public class ProcessEventsToDataTable extends DownloadAll<EventDto> {
     private int counter = 0;
     private MyDataTable dataTable;
     private EventsRequest request;
+    private IAstRcsWcfSvc service;
 
     public ProcessEventsToDataTable(EventsRequest request) {
         super(EventsResponse.class);
         this.request = request;
         dataTable = new MyDataTable();
+
+        java.net.Authenticator.setDefault(new NtlmAuthenticator("steltecia\\PublicServices", "FiitSvc123."));
+        service = new AstRcsWcfSvc().getPort(IAstRcsWcfSvc.class);
         test();
         start();
     }
 
     public void test() {
-        java.net.Authenticator.setDefault(new NtlmAuthenticator("steltecia\\PublicServices", "FiitSvc123."));
-        AstRcsWcfSvc tester = new AstRcsWcfSvc();
-        IAstRcsWcfSvc service = tester.getPort(IAstRcsWcfSvc.class);
-
         GetUserRequest req = new GetUserRequest();
         req.setUserId(1);
         GetUserResponse resposne = service.getUser(req);
@@ -81,11 +80,85 @@ public class ProcessEventsToDataTable extends DownloadAll<EventDto> {
         }
 
         RcsServerDto rcsServer = dokument.getRcsServer();
-        if (rcsServer == null) { // tzv ide o lokalny subor bez riadeniaverzii
+        if (rcsServer == null) { // tzv ide o lokalny subor bez riadenia verzii
             logger.info("Lokalny subor");
             return;
         }
         String fragment = rcsServer.getTypeUri().getFragment();
+
+        Integer id = Integer.valueOf(dokument.getChangesetIdInRcs());
+        if (id == 0) { // changeset - teda commit id nenajdeny
+            logger.info("ChangesetId 0");
+            return;
+        }
+
+        try {
+            GetChangesetRequest req = new GetChangesetRequest();
+            req.setChangesetId(id);
+            GetChangesetResponse response;
+            response = service.getChangeset(req);
+            logger.info("GetChangesetRequest " + response.getChangeset().getValue().getCommitter().getValue().getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // deprecated GetCodeEntitiesOfFileByRawRcsArguments
+
+        try {
+            GetChangedFilesRequest req2 = new GetChangedFilesRequest();
+            req2.setChangesetId(id);
+            GetChangedFilesResponse response2;
+            response2 = service.getChangedFiles(req2);
+            logger.info("GetChangedFilesRequest " + response2.getFileVersions().getValue().getFileVersionDto().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            GetCodeEntityChangesetsRequest req3 = new GetCodeEntityChangesetsRequest();
+            req3.setEntityId(2);
+            GetCodeEntityChangesetsResponse response3;
+            response3 = service.getCodeEntityChangesets(req3);
+            logger.info("GetCodeEntityChangesetsRequest " + response3.getChangesets().getValue().getChangesetDto().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            GetFileChangesetsRequest req3 = new GetFileChangesetsRequest();
+            req3.setEntityId(2);
+            GetFileChangesetsResponse response3;
+            response3 = service.getFileChangesets(req3);
+            logger.info("GetFileChangesetsRequest " + response3.getChangesets().getValue().getChangesetDto().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            GetFileContentRequest req3 = new GetFileContentRequest();
+            req3.setVersionId(id);
+            GetFileContentResponse response3;
+            response3 = service.getFileContent(req3);
+            logger.info("GetFileContentRequest " + response3.getContent().getValue());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // document.ChangesetIdInRcs changeset ID unique within AST RCS system, in which the entity version has been created
+        // teraz je otazka co chceme sledovat, povodny kod?
+        // alebo chceme sledovat kod ktory sa uz upravil a poslal na server?
+
+        /*
+        DOLEZITA JE KONECNOST
+        tzv uzivatel skopiroval kod, prepisal ho ....... potom ale mohol skopirovat kod znovu a prepisat ho a to je ako keby zmena nad novym skopirovanim
+        cize prve hladanie moze ist az po udalost kedy bolo opat pouzite kopirovanie nad danou entitnou
+        */
+
+        // Ak vývojár často používa prehliadač (resp. portál) pri písaní svojho kódu, ako často je potom tento kód prepisovaný?
+        // - Pozerat sa na ako casto pouziva prehliadac ?
+        // Platí táto tendencia na všetky jeho kódy? ... filter podla suborov, respektive pohlad len na subory
+        // Platí to u každého iného vývojára rovnako?" ... vyberat podla vyvojara
+        // + zamerat sa na filtre podla obdobia aktivit
+        // Granularita na zaklade casu ... cize pozriem na jednu hodinu, co napisal za hodinu a co bolo v tomto kode prepisane ... blbost
 
         String description = action
                 + "<span class=\"more\"><pre>"
