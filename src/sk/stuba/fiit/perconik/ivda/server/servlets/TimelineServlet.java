5d62196533ac61e02dd45bbcbb8451aade3cd10c
Sekerak <sekys.sekys@gmail.com>
2014-08-21 18:10:27 +0200

    Upravene poznamky. Nastavenia datumov presunute do noveho suboru.

--- src/sk/stuba/fiit/perconik/ivda/server/servlets/TimelineServlet.java
+++ src/sk/stuba/fiit/perconik/ivda/server/servlets/TimelineServlet.java
@@ -9,7 +9,7 @@ import sk.stuba.fiit.perconik.ivda.server.process.ProcessEventsToDataTable;
 import sk.stuba.fiit.perconik.ivda.server.processes.PrintProcesses;
 import sk.stuba.fiit.perconik.ivda.uaca.client.EventsRequest;
 import sk.stuba.fiit.perconik.ivda.util.DateUtils;
-import sk.stuba.fiit.perconik.uaca.dto.ProcessesChangedSinceCheckEventDto;
+import sk.stuba.fiit.perconik.uaca.dto.ide.IdeCodeEventDto;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
@@ -34,42 +34,12 @@ public final class TimelineServlet extends DataSourceServlet {
      */
     private static DataTable generateDataTable(GregorianCalendar start, GregorianCalendar end, Integer width) {
         EventsRequest request = new EventsRequest();
+        request.setTime(start, end);
         ProcessEventsToDataTable process;
 
-        request.setTime(start, end);
         request.setUser("steltecia\\pzbell");
-        //request.setType(new IdeCodeEventDto(), "pastefromweb");
-        //request.setType(new IdeCheckinEventDto());
-        request.setType(new ProcessesChangedSinceCheckEventDto());
+        request.setType(new IdeCodeEventDto());
         process = new PrintProcesses(request);
-
-
-        //start = DateUtils.fromString("2014-08-06T01:00:00.000Z");
-        //end = DateUtils.fromString("2014-08-06T23:00:00.000Z");
-        //request.setTime(start, end);
-        //process = new ProcessWebTabEvents(request);
-
-
-        // PinballFX2 v procesoch :D
-//        start = DateUtils.fromString("2014-08-06T01:00:00.000Z");
-//        end = DateUtils.fromString("2014-08-06T23:00:00.000Z");
-//        request.setTime(start, end).setType(new ProcessesChangedSinceCheckEventDto());
-//        process = new ProcessAllProcess(request);
-
-        // udalost MonitoringStartedEventDto nastane tak raz za tyzden, raz za tyzden zapina pc?
-        // dokaz http://perconik.fiit.stuba.sk/UserActivity/api/useractivity?page=0&pagesize=100&timefrom=2014-08-03T01:00:00Z&timeTo=2014-08-06T23:00:00Z&EventTypeUri=http://perconik.gratex.com/useractivity/event/system/monitoringstarted&ExactType=true
-
-
-        // pzbell vymazal 8suborov, 8suborov pridal a potom cely vecer sa menili len procesi
-//        start = DateUtils.fromString("2014-07-03T00:00:01.000Z");
-//        end = DateUtils.fromString("2014-07-03T23:59:00.000Z");
-//        request.setTime(start, end).setUser("steltecia\\pzbell");
-//        process = new ProcessJobEvents(request);
-
-        //start = DateUtils.fromString("2014-07-03T00:00:01.000");
-        //end = DateUtils.fromString("2014-07-03T10:59:00.000");
-        //request.setTime(start, end).setUser("steltecia\\pzbell"); //.setType(new ProcessesChangedSinceCheckEventDto());
-        //process = new ProcessAsGroup(request);
 
         LOGGER.info("Start: " + DateUtils.toString(start) +
                 " end:" + DateUtils.toString(end) +
@@ -98,11 +68,8 @@ public final class TimelineServlet extends DataSourceServlet {
             start = DateUtils.fromString(request.getParameter("start"));
             end = DateUtils.fromString(request.getParameter("end"));
             width = Integer.valueOf(request.getParameter("width"));
-            //start = DateUtils.fromString("2014-03-01T08:00:00.000Z");
-            //end = DateUtils.fromString("2014-10-30T16:00:00.000Z");
-            //start = DateUtils.fromString("2013-03-01T08:00:00.000Z");
-            start = DateUtils.fromString("2014-06-01T08:00:00.000Z");
-            end = DateUtils.fromString("2014-10-03T16:00:00.000Z");
+            start = DateUtils.fromString("2014-07-21T08:00:00.000Z");
+            end = DateUtils.fromString("2014-07-21T16:00:00.000Z");
             width = 1012;
         } catch (Exception e) {
             throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
