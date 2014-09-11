ed403ba485090ed4cb4d52750ee4dff2ba0a2a53
Sekerak <sekys.sekys@gmail.com>
2014-09-11 13:48:32 +0200

    Doplnene https linky a favicon.

--- src/sk/stuba/fiit/perconik/ivda/util/Configuration.java
+++ src/sk/stuba/fiit/perconik/ivda/util/Configuration.java
@@ -3,7 +3,6 @@ package sk.stuba.fiit.perconik.ivda.util;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -42,8 +41,8 @@ public final class Configuration implements Serializable {
         CONFIG_DIR = System.getProperty("config.dir", defaultDir);
 
         // Prepare log4j
-        String log4jLoggingPropFile = new File(CONFIG_DIR, LOGGING_PROPERTIES_FILE).getAbsolutePath();
-        PropertyConfigurator.configureAndWatch(log4jLoggingPropFile, 30000L);
+        //String log4jLoggingPropFile = new File(CONFIG_DIR, LOGGING_PROPERTIES_FILE).getAbsolutePath();
+        //PropertyConfigurator.configureAndWatch(log4jLoggingPropFile, 30000L);
 
         try {
             context = JAXBContext.newInstance(Configuration.class);
